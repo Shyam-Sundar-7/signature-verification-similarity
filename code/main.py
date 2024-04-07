492e8de72b724eda6456e51063867125395fd873
Shyam-Sundar-7 <mailshyamsundar.2022@gmail.com>
2024-04-07 23:22:59 +0530

    main edited

--- code/main.py
+++ code/main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from PIL import Image
-from code.inference1 import Inference_prediction1
-from code.inference2 import Inference_prediction2
+from inference1 import Inference_prediction1
+from inference2 import Inference_prediction2
 
 # Define the Streamlit app
 def main():
     # Set up the Streamlit app
     st.set_page_config(page_title="Image Similarity App", page_icon=":guardsman:", layout="wide")
     st.title("Image Similarity App")
-
+    
     # Get the two images from the user
     col1, col2 = st.columns(2)
     img1 = col1.file_uploader("Upload Testing Signature", type=["jpg", "jpeg", "png"])
@@ -24,16 +24,16 @@ def main():
 
     # Compute the features and similarity score
     if img1 is not None and img2 is not None:
-        if st.button("Compute Similarity"):
+        if st.button("Compute Similarity Between Two images"):
             # Extract features from the images
-            inference1 = Inference_prediction1("saved_trained_model_ckpt/best_model.ckpt")
+            inference1 = Inference_prediction1("models/best_model1.ckpt")
             x1=inference1.predict(img1,img2)
-            inference2 = Inference_prediction2("saved_trained_model_ckpt/contro_model.ckpt")
+            inference2 = Inference_prediction2("models/best_model2.ckpt")
             x2=inference2.predict(img1,img2)
 
             # Display the similarity score
-            st.markdown("The Disimilarity score between the two images ")
-            st.markdown("The Larger the score, Higher the Disimilarity")
+            st.markdown("The Similarity score between the two images ")
+            st.markdown("The Larger the score, Higher the Similarity")
 
             col1, col2= st.columns(2)
             col1.metric(label="Siaseme Network with Binary cross entryopy", value=x1)
@@ -41,7 +41,7 @@ def main():
 
             st.markdown("The Testing Signature is")
             
-            if (x1+x2)/2 > 50:
+            if (x1+x2)/2 < 50:
                 st.markdown(
                 '<div style="background-color:red; padding:10px">'
                 '<h2 style="color:black;text-align:center;">Forged</h2>'
